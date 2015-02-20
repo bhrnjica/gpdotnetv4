fd8c3a85d7d82c08d5e2bd85d71c18c25c22ba81
bhrnjica_cp <SND\bhrnjica_cp>
2015-02-20 12:19:21 +0000

    GPdotNET v4.0 beta 1

---
+++ GPdotNET/GPdotNET.App/AboutGPdotNET.cs
@@ -0,0 +1,128 @@
+﻿//////////////////////////////////////////////////////////////////////////////////////////
+// GPdotNET Tree based genetic programming tool                                         //
+// Copyright 2006-2012 Bahrudin Hrnjica                                                 //
+//                                                                                      //
+// This code is free software under the GNU Library General Public License (LGPL)       //
+// See licence section of  http://gpdotnet.codeplex.com/license                         //
+//                                                                                      //
+// Bahrudin Hrnjica                                                                     //
+// bhrnjica@hotmail.com                                                                 //
+// Bihac,Bosnia and Herzegovina                                                         //
+// http://bhrnjica.wordpress.com                                                        //
+//////////////////////////////////////////////////////////////////////////////////////////
+using System;
+using System.Reflection;
+using System.Windows.Forms;
+using System.Drawing;
+using GPdotNET.Util;
+
+namespace GPdotNET.App
+{
+    partial class AboutGPdotNET : Form
+    {
+        public AboutGPdotNET()
+        {
+            InitializeComponent();
+            this.Text = String.Format("About {0}", AssemblyTitle);
+            this.labelProductName.Text = AssemblyProduct;
+            this.labelVersion.Text = String.Format("Version {0}", AssemblyVersion);
+            this.labelCopyright.Text = AssemblyCopyright;
+            this.labelCompanyName.Text = AssemblyCompany;
+            this.labelCompanyName.Text = "Bahrudin Hrnjica; bhrnjica@hotmail.com";
+            this.Icon = Extensions.LoadIconFromName("GPdotNET.App.Resources.gpdotnet_ico48.ico");
+            this.Load += new EventHandler(AboutGPdotNET_Load);
+            //Sample code for running agains MONO or .NET
+            //Type t = Type.GetType ("Mono.Runtime");
+            //if (t != null)
+            //     MessageBox.Show ("You are running with the Mono VM");
+            //else
+            //     MessageBox.Show ("You are running something else");
+        }
+
+        void AboutGPdotNET_Load(object sender, EventArgs e)
+        {
+
+            this.logoPictureBox.Image = Extensions.LoadImageFromName("GPdotNET.App.Resources.gpabout.png");
+        }
+
+        #region Assembly Attribute Accessors
+
+        public string AssemblyTitle
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
+                if (attributes.Length > 0)
+                {
+                    AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
+                    if (titleAttribute.Title != "")
+                    {
+                        return titleAttribute.Title;
+                    }
+                }
+                return System.IO.Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().CodeBase);
+            }
+        }
+
+        public string AssemblyVersion
+        {
+            get
+            {
+                return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            }
+        }
+
+        public string AssemblyDescription
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false);
+                if (attributes.Length == 0)
+                {
+                    return "";
+                }
+                return ((AssemblyDescriptionAttribute)attributes[0]).Description;
+            }
+        }
+
+        public string AssemblyProduct
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyProductAttribute), false);
+                if (attributes.Length == 0)
+                {
+                    return "";
+                }
+                return ((AssemblyProductAttribute)attributes[0]).Product;
+            }
+        }
+
+        public string AssemblyCopyright
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
+                if (attributes.Length == 0)
+                {
+                    return "";
+                }
+                return ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
+            }
+        }
+
+        public string AssemblyCompany
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
+                if (attributes.Length == 0)
+                {
+                    return "";
+                }
+                return ((AssemblyCompanyAttribute)attributes[0]).Company;
+            }
+        }
+        #endregion
+    }
+}
---
+++ GPdotNET/GPdotNET.App/Extensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace GPdotNET.App
+{
+    public static class Extensions
+    {
+        public static Image LoadImageFromName(string name)
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            var pic = asm.GetManifestResourceStream(name);
+            return Image.FromStream(pic);
+        }
+
+        public static Icon LoadIconFromName(string name)
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            var pic = asm.GetManifestResourceStream(name);
+            if (pic == null)
+                return null;
+            else
+                return new Icon(pic);
+        }
+    }
+}
